6a568826e5f7a17d13d8dff080085942a6d7af36
yuichi oyama <yuichi19991208@gmail.com>
2020-09-28 21:48:16 +0900

    画像のリサイズをできる様にした

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,5 +1,5 @@
 module FeedsHelper
-  def choose_new_or_edit
+  def feed_choose_new_or_edit
     if action_name == 'new' || action_name == 'create'
       confirm_feeds_path
     elsif action_name == 'edit'
